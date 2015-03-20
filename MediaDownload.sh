383b6c520a8228a4cb6313e19f4ad87474d41c05
Bristol Museums Galleries and Archives <mark.pajak@bristol.gov.uk>
2015-03-20 10:59:43 +0000

    Update MediaDownload.sh

--- MediaDownload.sh
+++ MediaDownload.sh
@@ -1,3 +1,6 @@
+# author: Mark Pajak
+# this script extracts events data and images only from the zipped digital signage folder. 
+
 
 time=$(date +%k%M)
 time=$(date +%k%M)
@@ -17,15 +20,15 @@ curl "https://dl.dropboxusercontent.com/u/244902320/Digital%20Signage.zip" -o "$
 # fi
 
 # unzip to a temporary directory
-mkdir -p /home/signagepc1/Desktop/temp
-unzip -o /home/signagepc1/Desktop/Digisign -d /home/signagepc1/Desktop/temp
+mkdir -p $HOME/Desktop/temp
+unzip -o $HOME/Desktop/Digisign -d $HOME/Desktop/temp
 
 # check the unzip worked
 if [ -f "$HOME/Desktop/temp/Digital Signage/index.html" ];
  then
 echo "index.html exists"
-rsync -va --delete "$HOME/Desktop/temp/Digital Signage/images" /home/signagepc1/Desktop/Digisign/ 
-rsync -va --delete "$HOME/Desktop/temp/Digital Signage/data" /home/signagepc1/Desktop/Digisign/ 
+rsync -va --delete "$HOME/Desktop/temp/Digital Signage/images" $HOME/Desktop/Digisign/ 
+rsync -va --delete "$HOME/Desktop/temp/Digital Signage/data" $HOME/Desktop/Digisign/ 
 rm -r -f "$HOME/Desktop/temp"
 else
 echo "no files to add - possible issue with content download!"
