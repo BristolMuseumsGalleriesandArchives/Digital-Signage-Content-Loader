eda48fb1687dba8db56c2763ce366291591dfae0
Bristol Museums Galleries and Archives <mark.pajak@bristol.gov.uk>
2015-03-20 11:02:36 +0000

    Update startChrome.sh

--- startChrome.sh
+++ startChrome.sh
@@ -1,6 +1,8 @@
+# author Mark Pajak (Bristol Museums Galleries and Archives)
+# this script starts google Chrome in Kiosk mode
 #!/bin/sh
 LOCATION=$(grep -Po '(?<="location": ")[^"]*' $HOME/scripts/settings.JSON)
 
 echo "$LOCATION"
-google-chrome-stable  "file:///home/signagepc1/Desktop/Digisign/index.html#/$LOCATION" --allow-file-access-from-files --kiosk --incognito
+google-chrome-stable  "file:///$HOME/Desktop/Digisign/index.html#/$LOCATION" --allow-file-access-from-files --kiosk --incognito
 
