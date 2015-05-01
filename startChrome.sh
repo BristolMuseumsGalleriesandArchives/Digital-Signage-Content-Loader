73f04b64f40538c3feb56b2fcadea84ba27a4067
JoelGG <joel.grimmer@gmail.com>
2015-05-01 16:51:40 +0100

    Update startChrome.sh

--- startChrome.sh
+++ startChrome.sh
@@ -1,8 +1,5 @@
-# author Mark Pajak (Bristol Museums Galleries and Archives)
-# this script starts google Chrome in Kiosk mode
 #!/bin/sh
 LOCATION=$(grep -Po '(?<="location": ")[^"]*' $HOME/scripts/settings.JSON)
 
 echo "$LOCATION"
-google-chrome-stable  "file:///$HOME/Desktop/Digisign/index.html#/$LOCATION" --allow-file-access-from-files --kiosk --incognito
-
+google-chrome-stable  "file:///$HOME/Desktop/Digisign/index.html#/events/$LOCATION" --allow-file-access-from-files --kiosk --incognito
